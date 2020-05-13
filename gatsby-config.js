877dfb0d5670380454aa3fe67fdeeedbbce73ee5
Aparna <aparna.joshi@citrix.com>
2020-05-13 22:29:08 +0530

    google-analytics-2

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-166276353-1",
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
