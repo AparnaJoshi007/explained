f6ec8bd510533e57920c9c51c4aac5622bf8ba9a
Aparna <aparna.joshi@citrix.com>
2020-05-13 22:32:36 +0530

    analytics-g-3

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,24 +113,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "aparnajoshi.netlify.app",
       },
     },
     "gatsby-plugin-offline",
