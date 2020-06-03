184d115ca1219afba82ac11c82342cca5c2ba526
Aparna <aparna.joshi@citrix.com>
2020-06-03 22:39:27 +0530

    lambda

---
+++ lambda/index.js
@@ -0,0 +1,28 @@
+const { REVUE_KEY } = process.env;
+import fetch from "node-fetch";
+
+const API_ENDPOINT = "https://www.getrevue.co";
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const { email } = JSON.parse(event.body) || "";
+  const { name } = JSON.parse(event.body) || "";
+
+  return  fetch(`${API_ENDPOINT}/api/v2/subscribers`, {
+            method: 'POST',  
+            headers: {
+                'Authorization': `Token ${REVUE_KEY}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "email": email,
+                "name": name,
+                "double_opt_in": true
+            })
+        })
+        .catch(error => ({ statusCode: 422, body: String(error) }));
+};
